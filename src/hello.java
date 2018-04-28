a1168aad783b8965133e14bb1b04deb462367a25
Jerry @Lin <jf_mejiar547@live.com>
2018-04-27 21:28:36 -0600

    Java

---
+++ src/hello.java
@@ -0,0 +1,12 @@
+import javax.swing.JOptionPane;
+
+/**
+ * hello
+ */
+public class hello {
+    public static void main(String[] args) {
+        JOptionPane.showMessageDialog(null, "Hello world from Ubuntu 18.04", "", 
+        JOptionPane.INFORMATION_MESSAGE);
+    }
+    
+}
